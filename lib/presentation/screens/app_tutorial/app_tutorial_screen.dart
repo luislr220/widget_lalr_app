5003c96c51a6071bebb21b8df90b2c367353443e
luislr220 <largoramirezluisarmando@gmail.com>
2025-06-03 20:27:09 -0600

    App tutorial

--- lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,10 +1,148 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 
-class AppTutorialScreen extends StatelessWidget {
+class SlideInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SlideInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slides = <SlideInfo>[
+  SlideInfo(
+    'Disfruta',
+    'Enim non dolore ipsum ullamco aliquip et.',
+    'assets/images/undraw_deliveries_2m9t.png',
+  ),
+  SlideInfo(
+    'Entrega',
+    'Proident adipisicing cupidatat quis tempor.',
+    'assets/images/undraw_on-the-way_ahi2.png',
+  ),
+  SlideInfo(
+    'Comida',
+    'Consectetur exercitation anim sint mollit exercitation laborum cupidatat ea aliqua dolor occaecat.',
+    'assets/lottie/Animation - 1748998297475.json',
+  ),
+];
+
+class AppTutorialScreen extends StatefulWidget {
   const AppTutorialScreen({super.key});
 
   @override
+  State<AppTutorialScreen> createState() => _AppTutorialScreenState();
+}
+
+class _AppTutorialScreenState extends State<AppTutorialScreen> {
+  final PageController pageViewController = PageController();
+
+  bool endReached = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pageViewController.addListener(() {
+      //print("${pageViewController.page}");
+      final page = pageViewController.page ?? 0;
+      if (!endReached && page >= slides.length - 1.5) {
+        setState(() {
+          endReached = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    pageViewController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageViewController,
+            physics: const BouncingScrollPhysics(),
+            children: slides
+                .map(
+                  (slideData) => _Slide(
+                    title: slideData.title,
+                    caption: slideData.caption,
+                    imageUrl: slideData.imageUrl,
+                  ),
+                )
+                .toList(),
+          ),
+          Positioned(
+            right: 20,
+            top: 50,
+            child: TextButton(
+              onPressed: () => context.pop(),
+              child: const Text('Salir'),
+            ),
+          ),
+          endReached
+              ? Positioned(
+                  bottom: 30,
+                  right: 30,
+                  child: FadeInRight(
+                    duration: const Duration(seconds: 1),
+                    child: FilledButton(
+                      onPressed: () {},
+                      child: const Text('Comenzar'),
+                    ),
+                  ),
+                )
+              : const SizedBox(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final captionStyle = Theme.of(context).textTheme.bodySmall;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            imageUrl.endsWith('json')
+                ? FadeInDown(child: Lottie.asset(imageUrl))
+                : FadeInDown(child: Image(image: AssetImage(imageUrl))),
+
+            FadeIn(
+              duration: const Duration(seconds: 2),
+              child: Text(title, style: titleStyle),
+            ),
+            Text(caption, style: captionStyle),
+          ],
+        ),
+      ),
+    );
   }
 }
