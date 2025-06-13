26dd1a0452fa2af6e294a00e1eaf5650d9867447
luislr220 <largoramirezluisarmando@gmail.com>
2025-06-13 16:15:09 -0600

    Agregando Progress Screen

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -21,6 +21,48 @@ class ProgressScreen extends StatelessWidget {
 class _ProgressView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return const Center(
+      child: Column(
+        children: [
+          Text('Circular Progress Indicator'),
+          SizedBox(height: 20),
+          CircularProgressIndicator(
+            strokeWidth: 2,
+            backgroundColor: Colors.black38,
+          ),
+          SizedBox(height: 20),
+          Text('Circular y Linear controlado'),
+          _ControllerProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  const _ControllerProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 1),
+      builder: (context, snapshot) {
+        final progressValue = snapshot.data ?? 0;
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(
+              value: progressValue,
+              strokeWidth: 2,
+              backgroundColor: Colors.black45,
+            ),
+            const SizedBox(width: 20),
+            Expanded(child: LinearProgressIndicator(value: progressValue)),
+          ],
+        );
+      },
+    );
   }
 }
